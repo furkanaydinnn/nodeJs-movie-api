b6fda45193640eea4168f7f03c8ef791047e6930
Furkan Aydın <furkanaydin@Furkans-MacBook-Pro.local>
2020-01-11 22:03:22 +0300

    movie api dev.

--- models/Movie.js
+++ models/Movie.js
@@ -11,7 +11,7 @@ const MovieSchema = new Schema({
     country: String,
     year: Number,
     imdb_score: Number,
-    date: {
+    createdIn: {
         type: Date,
         default: Date.now()
     }
--- routes/movie.js
+++ routes/movie.js
@@ -5,7 +5,80 @@ const router = express.Router();
 const Movie = require('../models/Movie');
 
 router.get('/', (req, res, next)=> {
-  res.json({status : 200});
+    const promise = Movie.find({ });
+
+    promise.then((data) =>{
+       res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+// get top 10 list
+router.get('/top10',(req,res,next)=>{
+    const promise = Movie.find({}).limit(10).sort({imdb_score: -1});
+
+    promise.then((data) => {
+        if(!data)
+            next({ message: 'No movie found.', code: 98 });
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+// between two specified years.
+router.get('/between/:start_year/:end_year',(req,res,next)=>{
+    const {start_year, end_year} = req.params;
+    const promise = Movie.find(
+        {
+            year : {"$gte": parseInt(start_year), "$lte":parseInt(end_year)}
+        }
+    );
+
+    promise.then((data) => {
+        if(data.isEmpty())
+            next({ message: 'No movie found between specified dates.', code: 99 });
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.get('/:movie_id',(req,res,next)=>{
+    const promise = Movie.findById(req.params.movie_id);
+
+    promise.then((movie) => {
+        if(!movie)
+            next({ message: 'The movie was not found.', code: 99 });
+        res.json(movie);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.put('/:movie_id',(req,res,next)=>{
+    const promise = Movie.findByIdAndUpdate(req.params.movie_id,req.body, {new : true});
+
+    promise.then((movie) => {
+        if(!movie)
+            next({ message: 'The movie is not defined.', code: 99 });
+        res.json(movie);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.delete('/:movie_id',(req,res,next)=>{
+    const promise = Movie.findByIdAndRemove(req.params.movie_id);
+
+    promise.then((movie) => {
+        if(!movie)
+            next({ message: 'The movie is not defined.', code: 99 });
+        res.json(movie);
+    }).catch((err) => {
+        res.json(err);
+    });
 });
 
 router.post('/', (req, res, next)=> {
